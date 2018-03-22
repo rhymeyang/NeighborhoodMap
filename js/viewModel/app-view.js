0c7cebd654de98b9bfa158de4b7f0cc652106352
vicky <29121884@qq.com>
2018-03-22 22:34:27 +0800

    use input filter instead of select list

--- js/viewModel/app-view.js
+++ js/viewModel/app-view.js
@@ -4,13 +4,52 @@ var app = app || {};
 (function($){
     app.ViewModel = {
     currentCity: ko.observable(),
-    // areaFilter : ko.observable("选择感兴趣的地点"),
+    areaFilter : ko.observable(),
+    cleanFilter: function(){
+        this.areaFilter('');
+        for(let item of app.ViewModel.markArray()){
+            item.mark.setMap(app.map.mapObj);
+        }
+        // app.map.mapObj.setCenter([121.476200, 31.229670])
+    },
     selectedLocation: ko.observable(),
     locations: ko.observableArray(),
     markArray: ko.observableArray(),
     selectChanged: function(data){
         app.ViewModel.selectLocation(app.ViewModel.selectedLocation());
     },
+    filterLocations: ko.computed(function(){
+        if ( app.ViewModel.areaFilter()){
+             let re = new RegExp(app.ViewModel.areaFilter(),'i');
+            let result = [];
+
+            let ids = [];
+            for(let location of app.ViewModel.locations() ){
+                if(location.title.match(re)){
+                    result.push(location);
+                    ids.push(location.id);
+                }
+            }
+            if (result.length >0){
+                app.map.mapObj.setCenter([result[0].location.lng, result[0].location.lat])
+            }
+            for(let item of app.ViewModel.markArray()){
+                if (ids.includes(item.id)){
+                    console.log(`set mark ${item.id}`);
+                    item.mark.setMap(app.map.mapObj);
+                } else{
+                    item.mark.setMap(null);
+                }
+            }
+
+            return result;
+        }
+        else {
+
+            return app.ViewModel.locations();
+        }
+
+    }, this, {pure: true}),
     selectLocation: function(data){
         if (typeof(data) === 'undefined'){
             return;
